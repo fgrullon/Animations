2d529f748d5f95f462b0f102594420173ee41810
fgrullon <franfelix@gmail.com>
2024-07-29 19:25:15 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Frank Grullon on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
